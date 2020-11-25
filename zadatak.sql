1f9f95955fa157aea4bdb779945afe3c1c09183f
marinknezevic10 <73716434+marinknezevic10@users.noreply.github.com>
2020-11-25 14:46:06 +0100

    Add files via upload

---
+++ zadatak.sql
@@ -0,0 +1,37 @@
+drop database if exists zadatak;
+create database zadatak CHARACTER SET utf8;
+use zadatak;
+
+create table opcina(
+sifra int not null primary key auto_increment,
+zupanija int,
+naziv varchar(70) not null
+);
+
+create table zupanija(
+sifra int not null primary key auto_increment,
+naziv varchar(70) not null,
+zupan varchar(70)
+);
+
+
+alter table opcina add foreign key(zupanija) references zupanija(sifra);
+
+insert into opcina(sifra,naziv)
+values(null,'Qwer'),(null,'Qwert'),(null,'Qwertz');
+
+insert into opcina(sifra,naziv)
+values(null,'Asdf'),(null,'Asdfg'),(null,'Asdfgh');
+select * from opcina;
+
+insert into zupanija(sifra,naziv,zupan) 
+values(null,'Naziv','Pero'),
+	(null,'Zivna','Rope'),
+	(null,'Aha','Da');
+select * from zupanija;
+
+
+
+
+		
+	  
