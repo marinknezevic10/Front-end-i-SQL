56ea4b15a9b6edbbdd09a22c1c08502e3b048731
marinknezevic10 <73716434+marinknezevic10@users.noreply.github.com>
2021-01-25 18:52:13 +0100

    Delete headpb.php

--- headpb.php
+++
@@ -1,9 +0,0 @@
-<head>
-<meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $naslov; ?></title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-    </head>
-    
