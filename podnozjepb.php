80d4a60832f8b86452f20ee1960eb2a6b9ac1ed2
marinknezevic10 <73716434+marinknezevic10@users.noreply.github.com>
2021-01-25 18:54:53 +0100

    Delete podnozjepb.php

--- podnozjepb.php
+++
@@ -1,8 +0,0 @@
-<!--početak podnozje -->
-<hr/>  
-<footer>
-<link rel="stylesheet" href="css/oke.css">
-  
-<p class="dolje">Privatnost</p> <p class="dolje">FAQ</p> <p class="dolje">Pronašli ste grešku?</p>
-</footer>
-    <!--kraj podnozje -->
